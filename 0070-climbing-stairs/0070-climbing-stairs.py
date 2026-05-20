83dc7c8ba557f0e587fc8325c81276888708fe1f
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-20 16:30:06 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 19.1 MB (85.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,3 +1,21 @@
+# tabulation with space optimization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        prev2 = 1; prev1 = 2; curr =2 
+        for i in range(3,n+1):
+            curr = prev2 + prev1
+            temp = prev1
+            prev1 = curr
+            prev2 = temp
+        return curr
+
+"""
 # tabulation
 class Solution:
     def climbStairs(self, n: int) -> int:
@@ -9,7 +27,7 @@ class Solution:
         ways[2] = 2
         for i in range(3,n+1):
             ways[i] = ways[i-1] + ways[i-2]
-        return ways[n]
+        return ways[n] """
 
 '''
 # memoization
