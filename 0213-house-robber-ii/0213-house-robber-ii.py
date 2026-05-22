a411d93339e3258c30a45d30e5e3521970da09b8
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-22 21:57:59 +0530

    0213-HOUSE-ROBBER-II Stats: Time: 0 ms (100%), Space: 19.3 MB (31.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,5 +1,36 @@
-# Memoization on Recursion wala Approach
+# Tabulation 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+        dp = [-1]*n
+        # leave last one and put remaing
+        dp[0]= nums[0]
+        start = 0; last = n-2 # Boundary
+        for i in range(start+1,last+1):
+            pick = nums[i]
+            if i > 1:
+                pick += dp[i-2]
+            not_pick = dp[i-1]
+            dp[i] = max(pick,not_pick)
+        ans1 = dp[n-2]
 
+        # leave 1st and put remaining
+        dp = [-1]*n
+        dp[0] = 0; dp[1] = nums[1]
+        start = 1; last = n-1 # boundary
+        for i in range(start+1,last+1):
+            pick = nums[i]
+            if i > 1:
+                pick += dp[i-2]
+            not_pick = dp[i-1]
+            dp[i] = max(pick,not_pick)
+        ans2 = dp[n-1]
+        return max(ans1,ans2)
+
+# Memoization on Recursion wala Approach
+'''
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
@@ -26,4 +57,4 @@ class Solution:
         dp1 = [ -1]*(n)
         ans2 = find_seq(dp1,nums,len(nums)-1,0) # that 1 is starting boundary
         return max(ans1,ans2)
-        
+        '''
