4b3b6c1ab29b10d45add2f3f5e94a7741fdce56f
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-27 14:59:54 +0530

    3405-COUNT-THE-NUMBER-OF-SPECIAL-CHARACTERS-II Stats: Time: 261 ms (55.3%), Space: 30.2 MB (5.29%) - LeetHub

---
+++ 3405-count-the-number-of-special-characters-ii/3405-count-the-number-of-special-characters-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        n = len(word)
+        visited = [0]*(n)
+        freq = {} # key, [idx]
+        for idx,val in enumerate(word):
+            if val in freq:
+                freq[val].append(idx)
+            else:
+                freq[val] = [idx]
+        count = 0
+        low, high = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        for char1, char2 in list(zip(low,high)):
+            if char1 in freq and char2 in freq:
+                list1,list2 = freq[char1],freq[char2]
+                if list1 and list2:
+                    temp = True
+                    for e in list1:
+                        if e > list2[0]:
+                            temp = False
+                    if temp:
+                        count += 1
+        return count
