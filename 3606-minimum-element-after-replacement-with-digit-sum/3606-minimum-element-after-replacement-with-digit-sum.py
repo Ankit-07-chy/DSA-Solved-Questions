7a241aa97d69130019c68d4b6c1a7851f2024dea
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-29 08:02:07 +0530

    3606-MINIMUM-ELEMENT-AFTER-REPLACEMENT-WITH-DIGIT-SUM Stats: Time: 45 ms (6.88%), Space: 19.3 MB (56.38%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
@@ -1,5 +1,17 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
+        mini = float('inf'); n = len(nums)
+        for i in range(n):
+            val = nums[i]
+            curr = 0
+            while val:
+                curr += (val %10)
+                val = val // 10
+            mini = min(mini,curr)
+        return mini
+'''
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
         def find(number):
             number = str(number)
             curr = 0
@@ -9,4 +21,4 @@ class Solution:
         for idx,val in enumerate(nums):
             temp = val
             nums[idx] = find(temp)
-        return min(nums)
+        return min(nums)'''
