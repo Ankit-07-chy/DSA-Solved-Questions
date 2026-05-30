4394b7a0674f5afc825885b4f4b98a37417cceff
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-30 21:44:11 +0530

    0189-ROTATE-ARRAY Stats: Time: 8 ms (32.8%), Space: 26.6 MB (44.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,4 +1,32 @@
-# This codes gives TLE
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        
+        n = len(nums)
+        k = k%n
+        def reverse(arr,left,right):
+            while left < right:
+                arr[left],arr[right]=arr[right],arr[left]
+                left += 1
+                right -= 1
+        reverse(nums,0,n-1)
+        reverse(nums,0,k-1)
+        reverse(nums,k,n-1)
+
+# it will give TLE as O(n*K)
+"""
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+        for i in range(k):
+            last = nums[-1]
+            for j in range(n-1,0,-1):
+                nums[j] = nums[j-1]
+            nums[0] = last
+"""
+
+# using extra space - But I assume there should Be a mathematical way by which we can easily do it
+'''
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -13,4 +41,4 @@ class Solution:
             arr.append(nums[i])
         print(arr)
         for i in range(n):
-            nums[i] = arr[i]
+            nums[i] = arr[i]'''
