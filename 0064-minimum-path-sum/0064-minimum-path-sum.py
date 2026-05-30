4a10e92dbd816b0845cfdd2f1e0ad768c8022500
Ankit-07-chy <ankit07chy@gmail.com>
2026-05-30 12:45:24 +0530

    0064-MINIMUM-PATH-SUM Stats: Time: 13 ms (36.66%), Space: 21.8 MB (35.2%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,4 +1,24 @@
+# Tabulation - Bottom Up
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid); n = len(grid[0])
+        dp = [[0]*(n+1) for i in range(m+1)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j ==0 :
+                    dp[i][j] = grid[i][j]
+                elif i > 0 and j < 1:
+                    dp[i][j] = grid[i][j] + dp[i-1][j]
+                elif i < 1 and j > 0:
+                    dp[i][j] = grid[i][j] + dp[i][j-1]
+                else:
+                    dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1])
+
+        return dp[m-1][n-1]
+                    
+
 # Recursion + memoization
+"""
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m= len(grid); n = len(grid[0])
@@ -13,7 +33,7 @@ class Solution:
                 p2 = find(dp,grid,row,col-1)+ grid[row][col]
                 dp[row][col] = min(p1,p2)
             return dp[row][col]
-        return find(dp,grid,m-1,n-1)
+        return find(dp,grid,m-1,n-1)"""
 
 
 # recursion Approach with TLE
