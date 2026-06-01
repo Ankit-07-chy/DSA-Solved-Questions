e0b2b851a0132a1c6815f108d27b99e14da6b37f
Ankit-07-chy <ankit07chy@gmail.com>
2026-06-01 08:57:03 +0530

    0120-TRIANGLE Stats: Time: 11 ms (12.12%), Space: 20.4 MB (29.61%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -2,6 +2,20 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
+        dp = [[None]*(i+1) for i in range(n)]
+        def find(dp,row,col):
+            if row == n-1:
+                return triangle[row][col]
+            if dp[row][col] is None:
+                down = triangle[row][col] + find(dp,row+1,col)
+                diag = triangle[row][col]+find(dp,row+1,col+1)
+                dp[row][col] = min(down,diag)
+            return dp[row][col]
+        return find(dp,0,0)
+"""
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
         @cache
         def find(row,col):
             if row == n-1:
@@ -11,7 +25,7 @@ class Solution:
             diag = find(row+1,col+1)
             return triangle[row][col] + min(down,diag)
             
-        return find(0,0)
+        return find(0,0)"""
         
 
 # Recursion WIth TLE
