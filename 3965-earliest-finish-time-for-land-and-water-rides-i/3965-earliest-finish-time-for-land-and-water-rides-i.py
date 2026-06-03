c837451203c894b310b7aa796021b82b28ecd639
Ankit-07-chy <ankit07chy@gmail.com>
2026-06-03 17:49:13 +0530

    3965-EARLIEST-FINISH-TIME-FOR-LAND-AND-WATER-RIDES-I Stats: Time: 27 ms (53.76%), Space: 19.4 MB (57.8%) - LeetHub

--- 3965-earliest-finish-time-for-land-and-water-rides-i/3965-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/3965-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,4 +1,25 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        land = list(zip(landStartTime,landDuration))
+        water = list(zip(waterStartTime,waterDuration))
+        
+        land.sort(key = lambda x: x[0]+x[1])
+        water.sort(key=lambda x:x[0]+x[1])
+        print(land,water)
+        m1 = land[0][0] + land[0][1]
+        temp = inf
+        for k1,k2 in water:
+            temp = min(temp,(max(m1,k1)+k2))
+        
 
+        m2 = water[0][0] + water[0][1]
+        temp1 = inf
+        for k1,k2 in land:
+            temp1 = min(temp1,(max(m2,k1)+k2))
+        return min(temp,temp1)
+
+# Brute Force
+'''
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         n = len(landStartTime); m = len(waterStartTime)
@@ -13,4 +34,4 @@ class Solution:
             for j in range(0,n):
                 land_water = max(curr,landStartTime[j]) + landDuration[j]
                 result = min(result,land_water)
-        return result
+        return result'''
