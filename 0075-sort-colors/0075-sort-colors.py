57202483d289a86f4b98af4a1ab2082e7a0e4e5f
Ankit-07-chy <ankit07chy@gmail.com>
2026-06-06 14:41:06 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 19.3 MB (61.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,3 +1,27 @@
+# Optimal Approach
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 3 pointer
+        n = len(nums)
+        low = 0; mid = 0; high = n - 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[mid],nums[low] = nums[low],nums[mid]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid],nums[high]= nums[high],nums[mid]
+                high -= 1
+
+
+
+# Better Approach
+'''
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
@@ -16,4 +40,4 @@ class Solution:
         for i in range(zero_count,one_count+zero_count):
             nums[i] = 1
         for i in range(one_count+zero_count,one_count+zero_count+two_count):
-            nums[i] = 2
+            nums[i] = 2'''
