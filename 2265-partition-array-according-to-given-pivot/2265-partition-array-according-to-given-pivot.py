bb19dd75a3016c97919d379f1ec8e18a2604895b
Ankit-07-chy <ankit07chy@gmail.com>
2026-06-08 11:56:42 +0530

    2265-PARTITION-ARRAY-ACCORDING-TO-GIVEN-PIVOT Stats: Time: 44 ms (27.53%), Space: 34.1 MB (6.79%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
@@ -1,10 +1,38 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        '''
-            Every element less than pivot appears before and every element greater than pivot
-            every element equal to pivot appears in b/w
-            relative order should be maintain
-        '''
+        n = len(nums)
+        result = [pivot]*n
+        low = 0; high = n-1
+        for i in nums:
+            if i < pivot:
+                result[low] = i
+                low += 1
+            elif i > pivot:
+                result[high] = i
+                high -= 1
+        # reverse the array after the last pivot + 1 idx to n-1
+        last_idx = -1
+        for i in range(n-1,-1,-1):
+            if result[i] == pivot:
+                last_idx = i
+                break
+        high = n-1
+        last_idx += 1
+        while last_idx <= high:
+            result[last_idx],result[high]= result[high],result[last_idx]
+            last_idx += 1
+            high -= 1
+        return result
+
+# T.C. - O(n), S.C. - O(n)
+'''
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # 
+        #     Every element less than pivot appears before and every element greater than pivot
+        #     every element equal to pivot appears in b/w
+        #     relative order should be maintain
+        # 
         arr_left = []
         arr_right = []
         arr_bw = []
@@ -15,4 +43,4 @@ class Solution:
                 arr_left.append(u)
             if u > pivot:
                 arr_right.append(u)
-        return arr_left[:] + arr_bw[:] + arr_right[:]
+        return arr_left[:] + arr_bw[:] + arr_right[:]'''
