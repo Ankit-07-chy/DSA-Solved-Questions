75c76cb323aac0fbb23fb0d297e73c113a4d967c
Ankit-07-chy <ankit07chy@gmail.com>
2026-06-09 09:33:24 +0530

    0908-MIDDLE-OF-THE-LINKED-LIST Stats: Time: 0 ms (100%), Space: 19.1 MB (98.32%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# can use as len also, but that takes complexity O(n+n/2), in this case it use complexity o(N/2)
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
