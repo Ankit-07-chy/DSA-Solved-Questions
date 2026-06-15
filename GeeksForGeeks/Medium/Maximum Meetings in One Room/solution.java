750b232fa923ae5f84795de4eec3dfd17262d473
Ankit Kumar <ankit07chy@gmail.com>
2026-06-15 10:46:25 +0530

    Add maxMeetings method to schedule meetings
    Implement logic to find maximum meetings in one room.

--- GeeksForGeeks/Medium/Maximum Meetings in One Room/solution.java
+++ GeeksForGeeks/Medium/Maximum Meetings in One Room/solution.java
@@ -2,5 +2,30 @@ class Solution {
     public List<Integer> maxMeetings(int[] s, int[] f) {
         // code here
         
+        int n = s.length;
+        int[][] meetings = new int[n][3];
+        
+        for(int i =0; i < n; i++){
+            meetings[i][0] = s[i];
+            meetings[i][1] = f[i];
+            meetings[i][2] = i+1;
+        }
+        
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[1], b[1]));
+
+        List<Integer> ans = new ArrayList<>();
+
+        int freeTime = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (meetings[i][0] > freeTime) {
+                ans.add(meetings[i][2]);
+                freeTime = meetings[i][1];
+            }
+        }
+
+        Collections.sort(ans);
+        return ans;
+        
     }
 }
