6c4c50cc663d774d68346055dd99a1242ca25c76
Ankit Kumar <ankit07chy@gmail.com>
2026-06-19 08:46:42 +0530

    1833-FIND-THE-HIGHEST-ALTITUDE Stats: Time: 0 ms (100%), Space: 19.2 MB (85.71%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,7 +1,17 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        alt = 0
+        maxi = 0
+        for g in gain:
+            curr = alt + g
+            maxi = max(maxi,curr)
+            alt = curr
+        return maxi
+'''
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
         alt = [0]
         for g in gain:
             curr = alt[-1] + g
             alt.append(curr)
-        return max(alt)
+        return max(alt)'''
