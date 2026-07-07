52e3b34bd90dd13d882eaf7047af8bbd78139764
Ankit Kumar <ankit07chy@gmail.com>
2026-07-07 17:23:17 +0530

    0540-SINGLE-ELEMENT-IN-A-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 26.9 MB (60.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,3 +1,23 @@
+# o(ln(n)) --> approach
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0; right = n-1
+        if n == 1:
+            return nums[0]
+
+        while left < right:
+            mid = (left+right)//2
+            if mid % 2 == 1:
+                mid -= 1   
+            
+            if nums[mid] == nums[mid+1]:
+                left = mid + 2
+            else:
+                right = mid
+        return nums[left]
+
+"""
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         '''
@@ -5,6 +25,6 @@ class Solution:
         '''
         n = len(nums)
         ans = nums[0]
-        for i in range(1,n):
+        for i in range(1,n):  
             ans = ans^nums[i]
-        return ans
+        return ans"""
