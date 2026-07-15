b202fb4cdd4b67dfc7bd9059523f477c17cb9640
Ankit Kumar <ankit07chy@gmail.com>
2026-07-15 20:47:52 +0530

    1646-KTH-MISSING-POSITIVE-NUMBER Stats: Time: 0 ms (100%), Space: 19.2 MB (94.81%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -1,8 +1,28 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
+        low = 0
+        high = len(arr)-1
+        while low <= high:
+            mid = (low+high)//2
+            missed = arr[mid] - (mid+1)
+
+            if missed < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        miss = arr[high] - (high+1)
+        return low + k
+
+
+
+# Brute force
+'''
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
         
         # Brute force
         for i in arr:
             if i <= k:
                 k += 1
         return k
+        '''
