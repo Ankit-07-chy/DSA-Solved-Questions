08296a42e50c57ef3cf03a1d81dad17e8bd779b5
Ankit Kumar <ankit07chy@gmail.com>
2026-08-10 18:34:10 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 677 ms (22.09%), Space: 44.5 MB (44.8%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,9 +1,33 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1); n2 = len(text2)
+        dp = [[None]*(n2+1) for i in range(n1+1)]
+
+        def recursion(i,j):
+            if i >= n1 or j >= n2:
+                return 0
+
+            if dp[i][j] == None:
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + recursion(i+1,j+1)
+                    return dp[i][j]
+                else:
+                    t1 = recursion(i+1,j)
+                    t2 = recursion(i,j+1)
+                    dp[i][j] = max(t1,t2)
+                    return dp[i][j]
+            return dp[i][j]
+        
+        return recursion(0,0)
+
+
+'''
 from functools import cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         # 2D Dp Problem it is , lets do recursion first then will memoize it
         n1 = len(text1); n2 = len(text2)
-        
+
         @cache
         def recursion(i,j):
             if i >= n1 or j >= n2:
@@ -16,4 +40,4 @@ class Solution:
                 t2 = recursion(i,j+1)
                 return max(t1,t2)
                 
-        return recursion(0,0)
+        return recursion(0,0)'''
