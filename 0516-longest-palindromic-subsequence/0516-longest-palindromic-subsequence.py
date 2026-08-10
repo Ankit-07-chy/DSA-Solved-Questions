0fb39e0bbfb40a132575783bb24ad2066e8e9f06
Ankit Kumar <ankit07chy@gmail.com>
2026-08-10 22:14:53 +0530

    0516-LONGEST-PALINDROMIC-SUBSEQUENCE Stats: Time: 1279 ms (27.65%), Space: 35.8 MB (61.46%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,6 +1,28 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
+        ans = 0
+        dp = [[0]*(n+1) for i in range(n+1)]
+        if n == 1:
+            return 1
+        # For I we need loop in n-1 to -1 ; for j we need loop from 1 to n
+        for i in range(n-1,-1,-1):
+            for j in range(1,n):
+                if i > j:
+                    dp[i][j] = 0
+                elif s[i] == s[j] and i != j:
+                    dp[i][j] = 2 + dp[i+1][j-1]
+                elif s[i] == s[j]  and i == j:
+                    dp[i][j] = 1 + dp[i+1][j-1]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j-1])
+                ans = max(ans,dp[i][j])
+        return ans
+                
+'''
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
         
         dp = [[None]*(n+1) for i in range(n+1)]
         def recursion(i,j):
@@ -20,4 +42,4 @@ class Solution:
                     return dp[i][j]
             return dp[i][j]
         
-        return recursion(0,n-1)
+        return recursion(0,n-1)'''
