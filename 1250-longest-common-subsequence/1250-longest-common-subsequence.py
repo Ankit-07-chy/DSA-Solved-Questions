9ee58710b819978a9fb38d90e510080a0de1270d
Ankit Kumar <ankit07chy@gmail.com>
2026-08-10 18:52:30 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 306 ms (82.44%), Space: 44.2 MB (74.63%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,6 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n1 = len(text1); n2 = len(text2)
+        dp = [[0]*(n2+1) for i in range(n1+1)]
+
+        for i in range(n1-1,-1,-1):
+            for j in range(n2-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1+ dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
+
+# recursion + memoization
+"""
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1); n2 = len(text2)
         dp = [[None]*(n2+1) for i in range(n1+1)]
 
         def recursion(i,j):
@@ -19,7 +34,7 @@ class Solution:
             return dp[i][j]
         
         return recursion(0,0)
-
+"""
 
 '''
 from functools import cache
