5c776552d588e043d108d9988509128b79a2e40b
Ankit Kumar <ankit07chy@gmail.com>
2026-08-13 02:03:54 +0530

    0115-DISTINCT-SUBSEQUENCES Stats: Time: 407 ms (59.77%), Space: 76 MB (30.13%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,6 +1,24 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n = len(s); m = len(t)
+        dp = [[0]*(m+1) for i in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = 1
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][m]
+
+
+"""
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n = len(s); m = len(t)
         dp = [[None]*(m+1) for i in range(n+1)]
         def recursion(i,j):
             if j < 0:
@@ -16,7 +34,7 @@ class Solution:
                     dp[i][j] = recursion(i-1,j)
                 return dp[i][j]
         return recursion(n-1,m-1)
-
+"""
 
 # python inbuilt cache/ Memo
 '''
