c05b2e1842ae247ed4c9755529462cf1cd81784f
Ankit Kumar <ankit07chy@gmail.com>
2026-08-13 19:57:29 +0530

    0044-WILDCARD-MATCHING Stats: Time: 831 ms (25.52%), Space: 50.7 MB (28.5%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,3 +1,30 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n = len(s); m = len(p)
+        dp = [[False]*(m+1) for i in range(n+1)]
+
+        dp[0][0] = True
+        
+        for j in range(1,m+1):
+            if p[j-1] == '*':
+                dp[0][j] =  dp[0][j-1]
+
+        dp[0][0] = True
+        
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==p[j-1] or p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+                else:
+                    dp[i][j] = False
+        return dp[n][m]
+        
+
+
+"""
 # recursion + Memo
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
@@ -27,7 +54,7 @@ class Solution:
 
         return recursion(n-1,m-1)
 
-
+"""
 
 
 # python inbuild Cache
