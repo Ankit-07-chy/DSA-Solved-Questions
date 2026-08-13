ec93f5761679f6806bae4c7fa98cb4a8041cefda
Ankit Kumar <ankit07chy@gmail.com>
2026-08-13 11:28:25 +0530

    0072-EDIT-DISTANCE Stats: Time: 27 ms (94.25%), Space: 22.4 MB (84.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,9 +1,38 @@
+# recursion + memo
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1); m = len(word2)
+        dp = [[None]*(m+1) for i in range(n+1)]
+
+        def recursion(i,j):
+            if i < 0:
+                return j + 1
+            elif j < 0:
+                return i + 1
+            
+            elif word1[i] == word2[j]:
+                if dp[i][j] == None:
+                    dp[i][j] = recursion(i-1,j-1)
+                return dp[i][j]
+            else:
+                if dp[i][j] == None:
+                    insert = 1 + recursion(i,j-1)
+                    delete = 1 + recursion(i-1,j)
+                    replace = 1 + recursion(i-1,j-1)
+                    dp[i][j] = min([insert,delete,replace])
+                return dp[i][j]
+
+        return recursion(n-1,m-1)
+
+
+
+# Python inbuild Cache
+
+'''
 from functools import cache
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        '''
-        9 + 9 - 5 -5
-        '''
+       
         n = len(word1); m = len(word2)
 
         @cache
@@ -23,3 +52,4 @@ class Solution:
                 return min([insert,delete,replace])
 
         return recursion(n-1,m-1)
+        '''
