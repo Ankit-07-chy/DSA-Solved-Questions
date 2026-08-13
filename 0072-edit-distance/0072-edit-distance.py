9e2f5b952cbdc9d2356a85e4c8942d5b79fc3628
Ankit Kumar <ankit07chy@gmail.com>
2026-08-13 11:43:04 +0530

    0072-EDIT-DISTANCE Stats: Time: 71 ms (7.33%), Space: 22.7 MB (46.56%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,4 +1,24 @@
+# Tabulation
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1); m = len(word2)
+        dp = [[0]*(m+1) for i in range(n+1)]
+
+        # then for actual recursion
+        for i in range(0,n+1):
+            for j in range(0,m+1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] =  1 + min([dp[i-1][j],dp[i][j-1],dp[i-1][j-1]])
+        return dp[n][m]
+
 # recursion + memo
+"""
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n = len(word1); m = len(word2)
@@ -53,3 +73,4 @@ class Solution:
 
         return recursion(n-1,m-1)
         '''
+        """
