32e54312f8a2961ab7845e9209e83dd017aacb2a
Ankit Kumar <ankit07chy@gmail.com>
2026-08-14 17:10:14 +0530

    0122-BEST-TIME-TO-BUY-AND-SELL-STOCK-II Stats: Time: 58 ms (5.16%), Space: 31.9 MB (5%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,3 +1,27 @@
+# recursion + Memo
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[None]*2 for i in range(n)]
+
+        def f(idx,buy) :
+            # buy -> 1 and 0
+            if idx >= n:
+                return 0
+            if dp[idx][int(buy)] == None:
+                if buy :
+                    p1 = -prices[idx] + f(idx+1,False)
+                    p2 = f(idx+1,True)
+                else:
+                    p1 = prices[idx] + f(idx+1,True)
+                    p2 = f(idx+1,False)
+
+                dp[idx][int(buy)] = max(p1,p2)
+            
+            return dp[idx][int(buy)]
+        return f(0,True)
+
+'''
 from functools import cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
@@ -16,4 +40,4 @@ class Solution:
                 p2 = f(idx+1, False)
             
             return max(p1,p2)
-        return f(0,True)
+        return f(0,True)'''
