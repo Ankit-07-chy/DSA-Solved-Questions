dedfb3bbc26d5124ac64cb50453ce5ddd903d1ca
Ankit Kumar <ankit07chy@gmail.com>
2026-08-16 02:03:02 +0530

    0188-BEST-TIME-TO-BUY-AND-SELL-STOCK-IV Stats: Time: 65 ms (39.39%), Space: 24 MB (37.42%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,5 +1,22 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[0 for i in range(k+1)] for i in range(2)] for i in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+            for j in range(1,k+1):
+                buy = max(-prices[i]+dp[i+1][0][j], dp[i+1][1][j])
+                sell = max(prices[i]+dp[i+1][1][j-1], dp[i+1][0][j])
+
+                dp[i][1][j] = buy
+                dp[i][0][j] = sell
+        return dp[0][1][k]
+
+
+
+'''
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
         
         # dp like K(Transactions), Buy or Sold, idx
         n = len(prices)
@@ -21,3 +38,5 @@ class Solution:
                 dp[idx][buy][cap] = max(p1,p2)
             return dp[idx][buy][cap]
         return recursion(0,1,k)
+
+'''
