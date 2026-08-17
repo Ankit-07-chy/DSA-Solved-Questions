626eed97ff34a31c36bc9a9ae4ece96b17842e47
Ankit Kumar <ankit07chy@gmail.com>
2026-08-18 00:48:13 +0530

    1685-STONE-GAME-V Stats: Time: 385 ms (11.3%), Space: 47.7 MB (60.58%) - LeetHub

--- 1685-stone-game-v/1685-stone-game-v.java
+++ 1685-stone-game-v/1685-stone-game-v.java
@@ -1,46 +1,42 @@
-import java.util.Arrays;
-
 class Solution {
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
-        
-        // Prefix sums for O(1) range sum queries
-        int[] pref = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] + stoneValue[i];
-        }
-        
-        int[][] dp = new int[n][n];
-        
-        // Tabulation loop by subarray length
-        for (int length = 2; length <= n; length++) {
-            for (int i = 0; i <= n - length; i++) {
-                int j = i + length - 1;
-                int maxi = 0;
+        int[][] dp = new int[n + 1][n + 1];
+
+        // i from n-1 to 0 and j from i+1 to n-1
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+
+                // find total Sum
+                int totalSum = 0;
+                for (int k = i; k <= j; k++) {
+                    totalSum += stoneValue[k];
+                }
+
+                // now recursion wala part
+                int maxi = -1000000000;
+                int currSum = 0;
                 
                 for (int k = i; k < j; k++) {
-                    // Sum of left part: stoneValue[i...k]
-                    int leftSum = pref[k + 1] - pref[i];
-                    // Sum of right part: stoneValue[k+1...j]
-                    int rightSum = pref[j + 1] - pref[k + 1];
-                    
+                    currSum += stoneValue[k];
+
+                    int remSum = totalSum - currSum;
                     int s1 = 0, s2 = 0, s3 = 0;
-                    if (leftSum < rightSum) {
-                        s1 = leftSum + dp[i][k];
-                        maxi = Math.max(maxi, s1);
-                    } else if (leftSum > rightSum) {
-                        s2 = rightSum + dp[k + 1][j];
-                        maxi = Math.max(maxi, s2);
+                    
+                    if (remSum > currSum) {
+                        s1 = currSum + dp[i][k];
+                    } else if (remSum < currSum) {
+                        s2 = remSum + dp[k + 1][j];
                     } else {
-                        s3 = leftSum + Math.max(dp[i][k], dp[k + 1][j]);
-                        maxi = Math.max(maxi, s3);
+                        s3 = currSum + Math.max(dp[i][k], dp[k + 1][j]);
                     }
+                    
+                    maxi = Math.max(maxi, Math.max(s1, Math.max(s2, s3)));
                 }
                 
                 dp[i][j] = maxi;
             }
         }
-        
         return dp[0][n - 1];
     }
 }
