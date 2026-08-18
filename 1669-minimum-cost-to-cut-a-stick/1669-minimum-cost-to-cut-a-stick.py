6309cb694e7a952e5a6e2e103b970e0c82a9c9b6
Ankit Kumar <ankit07chy@gmail.com>
2026-08-18 20:08:49 +0530

    1669-MINIMUM-COST-TO-CUT-A-STICK Stats: Time: 791 ms (24.98%), Space: 34.6 MB (10.9%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.py
@@ -1,33 +1,57 @@
+from functools import cache
+
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        c = len(cuts)
-        cuts.append(0); cuts.append(n); cuts.sort()
-        @cache
-        def f(i,j):
-            if i > j:
-                return 0
-            maxi = 10**9 
-            for k in range(i,j+1):
-                cost = cuts[j+1] - cuts[i-1] + f(i,k-1) + f(k+1,j)
-                maxi = min(maxi,cost)
-            return maxi
-        return f(1,c)
 
+        @cache
+        def f(i, j):
+            if i >= j:
+                return 0
+
+            mini = float('inf')
+
+            for cut in cuts:
+                if i < cut < j:
+                    cost = (j - i) + f(i, cut) + f(cut, j)
+                    mini = min(mini, cost)
+                # No cut exists between i and j
+            if mini == float('inf'):
+                return 0
+
+            return mini
+
+        return f(0, n)
 
 # class Solution:
 #     def minCost(self, n: int, cuts: List[int]) -> int:
-#         cuts.sort()
-
+#         c = len(cuts)
+#         cuts.append(0); cuts.append(n); cuts.sort()
 #         @cache
 #         def f(i,j):
-#             if i >= j:
+#             if i > j:
 #                 return 0
-#             mini = 0
+#             maxi = 10**9 
+#             for k in range(i,j+1):
+#                 cost = cuts[j+1] - cuts[i-1] + f(i,k-1) + f(k+1,j)
+#                 maxi = min(maxi,cost)
+#             return maxi
+#         return f(1,c)
+
+
+# # class Solution:
+# #     def minCost(self, n: int, cuts: List[int]) -> int:
+# #         cuts.sort()
+
+# #         @cache
+# #         def f(i,j):
+# #             if i >= j:
+# #                 return 0
+# #             mini = 0
            
-#             for cut in cuts:
+# #             for cut in cuts:
                 
-#                 if i<cut<j:
-#                     cost = j-i + f(i,cut) + f(cut,j)
-#                     mini = min(mini,cost)
-#             return mini
-#         return f(0,n)
+# #                 if i<cut<j:
+# #                     cost = j-i + f(i,cut) + f(cut,j)
+# #                     mini = min(mini,cost)
+# #             return mini
+# #         return f(0,n)
