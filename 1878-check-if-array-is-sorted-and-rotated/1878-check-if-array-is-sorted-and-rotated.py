71b16dce5ec142d6f21551c2d7576badbe16031f
Ankit Kumar <ankit07chy@gmail.com>
2026-09-06 10:34:59 +0530

    1878-CHECK-IF-ARRAY-IS-SORTED-AND-ROTATED Stats: Time: 3 ms (8.71%), Space: 19.3 MB (15.87%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,29 +1,14 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
+        change = 0
         n = len(nums)
-        peak = 0
-        for i in range(n):
-            if nums[i] > nums[(i+1)%n]:
-                peak += 1
-        return peak <= 1
+        if n <= 2:
+            return True # n : 1, always sorted, n : 2 either sorted or rotated
 
-'''
-class Solution:
-    def check(self, nums: List[int]) -> bool:
-        # find the position for Crack, Put all those Element in an Array from crack Index to end  and 0 to crack Index, then check for sorted into that array
-        index_crack = 0
-        n = len(nums)
-        for i in range(n-1):
-            if nums[i+1] >= nums[i]:
-                continue
-            else:
-                index_crack = i+1
-                break
-        expected_array = []
-        expected_array.extend(nums[index_crack:])
-        expected_array.extend(nums[:index_crack])
-        for i in range(0,n-1):
-            if expected_array[i+1] < expected_array[i]:
-                return False
-        return True
-        '''
+        # n > 2 : 3,4,5,
+        for i in range(0,n):
+            if nums[i] > nums[(i+1)%n]:
+                change += 1
+        
+        print(change)
+        return change < 2
