269c37cd47bd1b583ad6a3d9ef92636e52d4d6cd
Ankit Kumar <ankit07chy@gmail.com>
2026-09-07 01:43:52 +0530

    2271-REARRANGE-ARRAY-ELEMENTS-BY-SIGN Stats: Time: 52 ms (47.33%), Space: 43.3 MB (36.03%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,5 +1,20 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
+        pos_idx = 0; neg_idx = 1
+        ans = [0]*len(nums)
+        for i in range(len(nums)):
+            if nums[i] >=0:
+                ans[pos_idx]= nums[i]
+                pos_idx += 2
+            else:
+                ans[neg_idx]= nums[i]
+                neg_idx += 2
+        return ans
+
+
+'''
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
         pos = []
         neg = []
         for num in nums:
@@ -13,3 +28,4 @@ class Solution:
             ans.append(pos[i])
             ans.append(neg[i])
         return ans
+'''
